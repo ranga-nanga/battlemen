9e4babe48b8911aa3fbf35728ffb75dee7bc6ab2
ab04191 <ab04191@X7009792.ctl.intranet>
2016-06-29 13:31:23 -0500

    Added utility and weapon subclasses, weapon is the super for club and sword

---
+++ src/equipment/Utility.java
@@ -0,0 +1,9 @@
+package equipment;
+
+public class Utility extends Item{
+
+	public Utility(String group, int amount){
+		super(group, String.valueOf(amount));
+	}
+		
+}
---
+++ src/equipment/Weapon.java
@@ -0,0 +1,10 @@
+package equipment;
+
+public class Weapon extends Item{
+	
+	public int dmgDie = 0;
+	
+	public Weapon(String group, String type){
+		super(group, type);
+	}
+}
